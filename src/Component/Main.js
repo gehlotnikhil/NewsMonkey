fb3b4369375768d164bf1cd3dfd2c7bf9b7d6273
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2023-09-09 16:41:08 +0530

    News Api fetch Working is solved

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 import MainItem from './MainItem'
 export default class Main extends Component {
+  articles = [];
+  constructor()
+  {
+    super();
+    console.log("This is a Constructor...");
+    this.state = {
+      articles : this.articles
+    }
+  }
+
+
+  async componentDidMount()
+  {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b9f5200616ae406ca17e088fa95938b8";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles});
+  }
   render() {
     return (
       <div className="container"> 
         <h1>This is a Heading</h1>
         <div className="row">
+        {this.state.articles.map((element)=>{
+          return (
           <div className="col-md-4">
-        <MainItem title = "This is a title" description = "This is a Description"/>
+        <MainItem title = {element.title} newsUrl = {element.url} imageUrl = {element.urlToImage} className="card-img-top" description = {element.description}/>
           </div>
-          <div className="col-md-4">
-        <MainItem title = "This is a title" description = "This is a Description"/>
-          </div>
-          <div className="col-md-4">
-        <MainItem title = "This is a title" description = "This is a Description"/>
-          </div>
+        ) })}
         </div>
       </div>
     )
--- src/Component/MainItem.js
+++ src/Component/MainItem.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 
 export default class MainItem extends Component {
   render() {
-    let {title,description} = this.props;
+    let {title,description, imageUrl, newsUrl} = this.props;
     return (
+      <div className="my-3">
       <div className="card" style={{width: '18rem'}}>
-         <img src="https://ichef.bbci.co.uk/news/1024/branded_news/1824A/production/_130709889_gettyimages-50681709.jpg" className="card-img-top" alt=""/>
+         <img src={imageUrl?imageUrl:"https://www.livemint.com/lm-img/img/2023/09/08/600x338/EMS_IPO_GMP_1694142665996_1694142673386.JPG"} className="card-img-top" alt=""/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title?title:""}...</h5>
+            <p className="card-text">{description?description:""}...</p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
           </div>
+      </div>
       </div>
     )
   }
